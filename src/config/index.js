1870c2d91b4c5d0fb9fe3b2fd18ea48feefb3703
amazingHuo <15710021168@163.com>
2024-05-07 00:04:35 +0800

    Faucet

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import zh from "./zh.json";
+// import zh from "./zh.json";
 import en from "./en.json";
 import zhTW from "./zh-TW.json"
 
